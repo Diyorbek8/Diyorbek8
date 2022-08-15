fac200d43a8618e26b7cbd7518a3af168b08bd94
Diyorbek <106571959+Diyorbek8@users.noreply.github.com>
2022-08-15 16:55:27 +0500

    Diyorbek

---
+++ calculator.py
@@ -0,0 +1,19 @@
+a=int(input("1-son="))
+b=int(input("2-son="))
+x=int
+c=a+b
+n=a-b
+m=a*b
+l=a/b
+o=a*a
+p=b*b
+k=a*a*a
+u=b*b*b
+i=c*c
+t=c*c*c
+print("qushuv=",c,
+"ayiru =",n
+,"multiplay=",m,
+"buluv=",l,
+"kvadrat1=",o,
+"kvadrat2=",p,"kub1=",k,"kub2=",u,"yiģinsini kv =",i,"yig.di . kubi.=",t)
